f44e4d4fa2e7f40125ecf148df2c32ec2269ce5b
foutouren <wangxinweijava@163.com>
2017-04-25 15:35:22 +0800

    增加git工具类

---
+++ biz-access-container/src/main/java/com/glsx/biz/access/container/utils/GitUtils.java
@@ -0,0 +1,16 @@
+package com.glsx.biz.access.container.utils;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Lenovo
+ * Date: 2017/4/25
+ * Time: 15:34
+ * <p>
+ * Description:
+ */
+public class GitUtils {
+
+    public static void main(String[] args) {
+        System.out.println("Git Utils...");
+    }
+}
