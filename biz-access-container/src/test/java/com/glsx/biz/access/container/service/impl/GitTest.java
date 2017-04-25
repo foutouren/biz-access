24e697534307eef0f429d4d5dcb7f1b4b1a46dbd
foutouren <wangxinweijava@163.com>
2017-04-25 15:35:13 +0800

    增加git工具类

---
+++ biz-access-container/src/test/java/com/glsx/biz/access/container/service/impl/GitTest.java
@@ -0,0 +1,16 @@
+package com.glsx.biz.access.container.service.impl;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Lenovo
+ * Date: 2017/4/25
+ * Time: 14:43
+ * <p>
+ * Description:
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("===");
+    }
+}
